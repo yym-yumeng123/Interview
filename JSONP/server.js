71badb9ad6994a06e6b9c8670c0855b51517fc0a
yym <1614527443@qq.com>
2017-09-25 22:06:23 +0800

    jsonp跨域案例

---
+++ JSONP/server.js
@@ -0,0 +1,35 @@
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+var url = require('url')
+
+http.createServer(function(req, res){
+  var pathObj = url.parse(req.url, true)
+
+  switch (pathObj.pathname) {
+    case '/getNews':
+      var news = [
+        "第11日前瞻：中国冲击4金 博尔特再战200米羽球",
+        "正直播柴飚/洪炜出战 男双力争会师决赛",
+        "女排将死磕巴西！郎平安排男陪练模仿对方核心"
+        ]
+      res.setHeader('Content-Type','text/json; charset=utf-8')
+      if(pathObj.query.callback){
+        res.end(pathObj.query.callback + '(' + JSON.stringify(news) + ')')
+      }else{
+        res.end(JSON.stringify(news))
+      }
+
+      break;
+
+    default:
+      fs.readFile(path.join(__dirname, pathObj.pathname), function(e, data){
+        if(e){
+          res.writeHead(404, 'not found')
+          res.end('<h1>404 Not Found</h1>')
+        }else{
+          res.end(data)
+        }
+      }) 
+  }
+}).listen(8080)
